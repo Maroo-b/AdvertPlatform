2943579e9e868b50be1c51291e57ed7871e51bb9
Marouen Bousnina <bousnina.marouen@gmail.com>
2016-03-05 12:32:24 +0100

    create Advert bundle

---
+++ README.md
@@ -0,0 +1,4 @@
+advert_patform
+==============
+
+A Symfony project created on March 5, 2016, 11:50 am.
---
+++ src/MB/PlatformBundle/MBPlatformBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MB\PlatformBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MBPlatformBundle extends Bundle
+{
+}
