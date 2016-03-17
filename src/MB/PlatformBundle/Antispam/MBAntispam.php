4cb8f3a6e09f49815d162501e973df014d66f695
Marouen Bousnina <marouen.it.technology@gmail.com>
2016-03-17 06:22:30 +0100

    Add listener for beta banner

--- src/MB/PlatformBundle/Antispam/MBAntispam.php
+++ src/MB/PlatformBundle/Antispam/MBAntispam.php
@@ -2,16 +2,36 @@
 
 namespace MB\PlatformBundle\Antispam;
 
-class MBAntispam
+class MBAntispam extends \Twig_Extension
 {
 
   private $minLength;
+  protected $msg;
   public function __construct($minLength)
   {
     $this->minLength = (int) $minLength;
   }
   public function isSpam($text)
   {
-    return strlen($text) < $this->minLength;
+    if(strlen($text) < $this->minLength){
+      return $this->msg;
+    }
+  }
+
+  public function getFunctions()
+  {
+    return array(
+      'checkIfSpam' => new \Twig_Function_Method($this, 'isSpam')
+    );
+  }
+
+  public function setMsg($msg)
+  {
+    $this->msg = $msg;
+  }
+
+  public function getName()
+  {
+    return 'MBAntispam';
   }
 }
---
+++ src/MB/PlatformBundle/Beta/BetaListener.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MB\PlatformBundle\Beta;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class BetaListener
+{
+  protected $betaHTML;
+
+}
